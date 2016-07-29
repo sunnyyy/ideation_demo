c29e3bbf6c730149fb63e95849ade77c4ce0eb13
Ye <yesunnia@amazon.com>
2016-07-29 00:19:21 -0400

    added stuff; almost done

--- share.js
+++ share.js
@@ -1,19 +1,30 @@
-$("#mtabs h4").click(function() {
-    //  First remove class "active" from currently active tab
-    $("#mtabs h4").removeClass('active');
+// $("#mtabs h4").click(function() {
+//     //  First remove class "active" from currently active tab
+//     $("#mtabs h4").removeClass('active');
 
-    //  Now add class "active" to the selected/clicked tab
-   $(this).addClass("active");
+//     //  Now add class "active" to the selected/clicked tab
+//    $(this).addClass("active");
 
-    //  Hide all tab content
-    $(".mtab_content").hide();
+//     //  Hide all tab content
+//     $(".mtab_content").hide();
 
-    //  Here we get the href value of the selected tab
-    var selected_tab = $(this).find("a").attr("href");
+//     //  Here we get the href value of the selected tab
+//     var selected_tab = $(this).find("a").attr("href");
 
-    //  Show the selected tab content
-    $(selected_tab).fadeIn();
+//     //  Show the selected tab content
+//     $(selected_tab).fadeIn();
 
-    //  At the end, we add return false so that the click on the link is not executed
-    return false;
-});
+//     //  At the end, we add return false so that the click on the link is not executed
+//     return false;
+// });
+function toggle_visibility() {
+    var a = document.getElementById('tab1');
+    var b = document.getElementById('tab2');
+    if (a.style.display == 'block') {
+        a.style.display = 'none';
+        b.style.display = 'block';
+    } else{
+        a.style.display = 'block';
+        b.style.display = 'none'
+    }
+}
